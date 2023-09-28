f06ebd5fd959d28b94e0b27d042a273f99dfb785
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-09-28 07:52:59 +0700

    fix: исправить данные

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const { PORT = 4001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
